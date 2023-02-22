c90d1d39d21a9556f1ab9866fbccc6b73d3c96f9
JoseAntMolina <42109988+jmoba@users.noreply.github.com>
2023-02-22 22:26:09 +0100

    demo inicial - Siempre latido, A: melodía

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("E B G A B G A F ", 90)
+    basic.pause(100)
+})
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
 })

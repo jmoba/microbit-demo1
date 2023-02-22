da771493bcc710c0c6459e958e76bbb2b25d346d
JoseAntMolina <42109988+jmoba@users.noreply.github.com>
2023-02-22 22:35:53 +0100

    A+B: Melodía, B: Giro

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.AB, function () {
     music.playMelody("E B G A B G A F ", 90)
     basic.pause(100)
+})
+input.onButtonPressed(Button.B, function () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 200)
+    basic.pause(1000)
+    maqueen.motorStop(maqueen.Motors.M2)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 200)
+    basic.pause(1000)
 })
 basic.forever(function () {
     basic.showIcon(IconNames.Heart)
